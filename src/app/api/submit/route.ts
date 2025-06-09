2f762d03fe54b948bfd27b360f9cd9a40e285281
Soh Hong Yu <sohhongyu@gmail.com>
2025-06-10 00:38:42 +0800

    Update route.ts

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -2,52 +2,48 @@ import { google } from "googleapis";
 import { NextResponse } from "next/server";
 import { numberToLetters } from "@/lib/utils";
 
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_SERVICE_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const sheets = google.sheets({ version: "v4", auth });
+
 export async function GET() {
-  return NextResponse.json({
-    message: "Submit API working perfectly!",
-  });
+  return NextResponse.json({ message: "Submit API working perfectly!" });
 }
 
 export async function POST(req: Request) {
   const { searchParams } = new URL(req.url);
-
   const submitType = searchParams.get("submitType");
-  console.log(submitType);
-  const data = await req.json();
 
+  const data = await req.json();
   const SHEET_ID = process.env.SHEET_ID!;
   const SHEET_NAME =
-    submitType === "survey"
-      ? process.env.SURVEY_SHEET_NAME || "FormResponses"
-      : process.env.SHEET_NAME || "FormResponses";
-
-  const auth = new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.GOOGLE_SERVICE_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
-
-  const sheets = google.sheets({ version: "v4", auth });
+    process.env[submitType === "survey" ? "SURVEY_SHEET_NAME" : "SHEET_NAME"] ||
+    "FormResponses";
 
   try {
     const timestamp = new Date().toLocaleString("en-US", {
       timeZone: "Asia/Singapore",
     });
+
+    const rowValues = [
+      timestamp,
+      ...Object.values(data).map((val) =>
+        Array.isArray(val) ? val.join(", ") : String(val)
+      ),
+    ];
+
     await sheets.spreadsheets.values.append({
       spreadsheetId: SHEET_ID,
-      range: `${SHEET_NAME}!A:${numberToLetters(Object.keys(data).length)}`,
+      range: `${SHEET_NAME}!A:${numberToLetters(rowValues.length)}`,
       valueInputOption: "USER_ENTERED",
       requestBody: {
-        values: [
-          [
-            timestamp,
-            ...Object.values(data).map((val) =>
-              Array.isArray(val) ? val.join(", ") : String(val)
-            ),
-          ],
-        ],
+        values: [rowValues],
       },
     });
 
